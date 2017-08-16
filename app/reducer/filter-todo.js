5317f64aa97c88564b058f81e7dcb587a1018654
rangelZZZ <18792561236@163.com>
2017-08-16 23:33:01 +0800

    refactor

--- app/reducer/filter-todo.js
+++ app/reducer/filter-todo.js
@@ -3,6 +3,7 @@ export default function filterTodos(state = {filterName: "ALL"}, action) {
     switch (action.type) {
         case "SET_FILTER": {
             state.filterName = action.filterName;
+            return state;
         }
 
         default: {
